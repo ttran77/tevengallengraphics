1e1407ecda1b0baf0af8ffc2faf8f6b0da6cf9c2
ttran77 <71350179+ttran77@users.noreply.github.com>
2022-04-07 05:16:01 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/* PERSONAL HOMEPAGE */
+
+console.log('Welcome to my personal homepage!')
+
+
+/* OFFSREEB MENU */
+function header(tgl = 'header' , cls = 'header') {
+	if (document.querySelector ('.${tgl}')) {
+		document.querySelector ('.${tg;}').
+		addEventListener('click'.
+		() => { document.body.classlist.toggle
+			(cls) }
+		)	
+			
+
+	}
+}
+
+
+
+
+ 
